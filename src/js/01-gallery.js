f2a9c70ca44fedf42ae51dd4393bfa75be19e199
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-05-12 14:51:59 +0300

    +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+
+const galleryList = document.querySelector(".gallery");
+
+// Create and render gallery items
+const createGalleryItem = ({ preview, original, description }) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}" data-original-img=${original}>
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>
+`;
+
+galleryList.innerHTML = galleryItems.map(createGalleryItem).join("");
+
+// Create instances of SimpleLightbox
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+});
+
+// Open the lightbox on image click
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+  lightbox.open();
+});
+
+
 console.log(galleryItems);
