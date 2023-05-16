483eb673892fcb5b21a743e42b9558ca042c3c46
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-05-16 08:47:59 +0300

    task  2 fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,35 +1,13 @@
-
-import '../css/common.css';
 import Player from '@vimeo/player';
-import { throttle } from 'lodash';
-
-const TIME_KEY = 'videoplayer-current-time';
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
 
-const onPlay = function (data) {
-  const strigifyData = JSON.stringify(data);
-  localStorage.setItem(TIME_KEY, strigifyData);
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
+
+const updateCurrentTime = data => {
+  localStorage.setItem(CURRENT_TIME, data.seconds);
 };
-player.on('timeupdate', throttle(onPlay, 1000));
 
-function resumePlayback() {
-  if (JSON.parse(localStorage.getItem(TIME_KEY)) === null) {
-    return;
-  }
-  const paused = JSON.parse(localStorage.getItem(TIME_KEY))['seconds'];
-  if (paused) {
-    player
-      .setCurrentTime(paused)
-      .then(function (seconds) {})
-      .catch(function (error) {
-        switch (error.name) {
-          case 'Error':
-            break;
-          default:
-            break;
-        }
-      });
-  }
-}
-resumePlayback();
+player.on('timeupdate', throttle(updateCurrentTime, 1000));
